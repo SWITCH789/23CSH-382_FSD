4d473eb6e461a50ce0b2cc251437060b0d1975cf
SWITCH789 <aayushdhar2005@gmail.com>
2026-02-05 09:47:52 +0530

    Minor modifications in experiment 3 files, added a README

---
+++ Exp3/README.md
@@ -0,0 +1,27 @@
+# Experiment 2: Implementing Routing in Single Page Applications (SPA).
+
+## Aim
+To implement basic client-side routing in a Single Page Application using React Router.
+
+## Tools & Libraries
+- React (Vite)
+- React Router Dom
+
+## Description
+This project demonstrates a single React application that includes:
+- A **Bootstrap Navbar**
+- A **Bootstrap Form**
+- A **Material UI Form**
+- **Material UI Card components**
+
+All components are organized into separate folders and rendered together in one application.
+
+## Screenshots
+
+### Experiment 3.1
+![alt text](../screenshots/31a.png)
+![alt text](../screenshots/31b.png)
+![alt text](../screenshots/31c.png)
+### Experiment 3.2, 3.3
+![alt text](../screenshots/32p.png)
+![alt text](../screenshots/32d.png)
--- Exp3/ex32/src/App.jsx
+++ Exp3/ex32/src/App.jsx
@@ -6,20 +6,20 @@ function Profile() {
   return (
     <div className="card profile-card landscape">
       <div className="profile-image">
-        <img src="exu.jpg" alt="Profile" />
+        <img src="ayus.jpg" alt="Profile" />
       </div>
 
       <div className="profile-details">
         <h1>Profile</h1>
-        <p><strong>Name:</strong> Name</p>
-        <p><strong>Email:</strong> sample@gmail.com</p>
-        <p><strong>Phone:</strong> 1234567890</p>
+        <p><strong>Name:</strong> Aayush Dhar</p>
+        <p><strong>Email:</strong> aayushdhar2005@gmail.com</p>
+        <p><strong>Phone:</strong> 9810919421</p>
       </div>
 
       <div className="projects profile-details">
         <h1>Projects</h1>
-        <p><strong>AI-based Patient Diagnosis Supoprt System And Medicine Recommendation</strong></p>
-        <p><strong>Innovative Shoes For Climbing Stairs</strong></p>
+        <p><strong>1. AI-based Patient Diagnosis Supoprt System And Medicine Recommendation</strong></p>
+        <p><strong>2. Innovative Shoes For Climbing Stairs</strong></p>
       </div>
 
     </div>
