a0b8ba017056b3183f68a2c3ed405e925df2cb61
SWITCH789 <aayushdhar2005@gmail.com>
2026-04-04 18:05:21 +0530

    Experiment 8 completed

--- Exp8/src/main/java/com/example/demo/service/StudentService.java
+++ Exp8/src/main/java/com/example/demo/service/StudentService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,5 +26,29 @@ public class StudentService {
     @SuppressWarnings("deprecation")
 	public Student getStudentById(int id) {
     	return repository.getById(id);
+    }
+
+    // UPDATE
+    public Student updateStudent(int id, Student student) {
+        Optional<Student> existingStudent = repository.findById(id);
+
+        if (existingStudent.isPresent()) {
+            Student updatedStudent = existingStudent.get();
+            updatedStudent.setName(student.getName());
+            updatedStudent.setEmail(student.getEmail());
+            return repository.save(updatedStudent);
+        } else {
+            return null; // you can also throw exception here
+        }
+    }
+
+    // DELETE
+    public String deleteStudent(int id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return "Student deleted successfully";
+        } else {
+            return "Student not found";
+        }
     }
 }
